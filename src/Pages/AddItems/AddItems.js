7a5bea3436a19a667da1cb6ec56427b003a5e112
MD SIFAT KHAN <sifatkhann229@gmail.com>
2022-05-08 23:39:22 +0800

    add item done

--- src/Pages/AddItems/AddItems.js
+++ src/Pages/AddItems/AddItems.js
@@ -1,39 +1,71 @@
+import axios from 'axios';
 import React from 'react';
-import { useForm } from "react-hook-form";
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { toast, ToastContainer } from 'react-toastify';
+import '../../firebase.init'
+import auth from '../../firebase.init';
 
 const AddItems = () => {
 
-    const { register, handleSubmit } = useForm();
+    const [user] = useAuthState(auth);
 
-    const onSubmit = data => {
-        console.log(data)
-        const url = `http://localhost:5000/inventory`;
-        fetch(url, {
-            method: 'POST',
-            headers: {
-                'content-type' : 'application/json'
-            },
-            body: JSON.stringify(data)
+    const handleAddItem = event => {
+        event.preventDefault();
+
+        const myitem = {
+            product_name: event.target.ProductName.value,
+            price: event.target.Price.value,
+            description: event.target.Description.value,
+            quantity: event.target.Quantity.value,
+            supplier_name: event.target.SupplierName.value,
+            img: event.target.PhotoURL.value,
+            email: user?.email
+        }
+
+        axios.post('http://localhost:5000/inventory', myitem)
+        .then(response => {
+            const {data} = response;
+            if(data.insertedId){
+                toast('Item Added');
+                event.target.reset();
+            }
         })
-        .then(res => res.json())
-        .then(result => {
-            console.log(result);
-        })
-        toast('Item Added');
+
+        // const url = `http://localhost:5000/inventory`;
+        // fetch(url, {
+        //     method: 'POST',
+        //     headers: {
+        //         'content-type' : 'application/json'
+        //     },
+        //     body: JSON.stringify(myitem)
+        // })
+        // .then(res => res.json())
+        // .then(result => {
+        //     console.log(result);
+        // })
+        // toast('Item Added');
     };
 
     return (
         <div className='container text-center'>
             <h2 className='mt-5'>Add Your Item</h2>
             <div className='w-50 mx-auto mt-4'>
-                <form className='d-flex flex-column' onSubmit={handleSubmit(onSubmit)}>
-                    <input className='mb-3' placeholder='Product Name' {...register("product_name", { required: true, maxLength: 20 })} />
-                    <input className='mb-3' placeholder='Supplier Name' {...register("supplier_name")} />
-                    <textarea className='mb-3' placeholder='Description' {...register("description")} />
-                    <input className='mb-3' placeholder='Price' type="number" {...register("price")} />
-                    <input className='mb-3' placeholder='Quantity' type="number" {...register("quantity")} />
-                    <input className='mb-3' placeholder='Photo URL' {...register("img")} />
+                <form className='d-flex flex-column' onSubmit={handleAddItem}>
+
+                    <input className='mb-3' type="text" name="ProductName" placeholder='ProductName' autoComplete='off' required/>
+
+                    <input className='mb-3' type="text" autoComplete='off' name="SupplierName" placeholder='SupplierName' required />
+
+                    <textarea className='mb-3' type="textarea" name="Description" placeholder='Description' required />
+
+                    <input className='mb-3' type="number" name="Price" placeholder='Price' required />
+
+                    <input className='mb-3' type="number" name="Quantity" placeholder='Quantity' required />
+
+                    <input className='mb-3' type="text" name="PhotoURL" autoComplete='off' placeholder='PhotoURL' required/>
+
+                    <input className='mb-3' type="email" name="email" placeholder='email' value={user?.email} readOnly />
+
                     <input type="submit" value="Add Item" />
                 </form>
                 <ToastContainer></ToastContainer>
